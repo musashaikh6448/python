c5c15bfb69cde12aaed154d7997a5a8644fdc9c9
shaikhmusa6448@gmail.com <shaikhmusa6448@gmail.com>
2024-10-18 19:23:31 +0530

    write code

--- flaskwebapp/index.py
+++ flaskwebapp/index.py
@@ -8,21 +8,27 @@ app = Flask(__name__)
 def postreq():
     print(request.get_json())
     return 'user created'
-@app.route('/home') 
-def diplay():
+@app.route('/index') 
+def index():
     return render_template('index.html')
+@app.route('/about') 
+def about():
+    return render_template('about.html')
+@app.route('/home') 
+def home():
+    return render_template('home.html')
 
 \
 
-@app.route('/contactus')
-def contact():
-    return "call us at mobile number is 7842363997"
-@app.route('/aboutus')
-def aboutus():
-    return "we are small start up company where provide training "
-@app.route('/services')
-def services():
-    return "mern sd product"
+# @app.route('/contactus')
+# def contact():
+#     return "call us at mobile number is 7842363997"
+# @app.route('/aboutus')
+# def aboutus():
+#     return "we are small start up company where provide training "
+# @app.route('/services')
+# def services():
+#     return "mern sd product"
 
 # Run the Flask application
 if __name__ == '__main__':
