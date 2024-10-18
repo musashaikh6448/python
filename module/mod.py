9e8766483c3a5828d0249ffb1329de563845700c
shaikhmusa6448@gmail.com <shaikhmusa6448@gmail.com>
2024-10-18 15:05:44 +0530

    change

--- module/mod.py
+++ module/mod.py
@@ -1,2 +1,8 @@
 def greeting(name):
     print("hello, " + name )
+
+
+    def display(msg):
+        print(f"your message is {msg}")
+
+    print ('i am comming from mod')
--- module/test.py
+++ module/test.py
@@ -1,10 +1,21 @@
-import mod  as mm
-import platform
+# import mod  as mm
+# import platform
 
 
-mm.greeting("musa")
+# mm.greeting("musa")
 
 # mod.greeting("shaikh")
 
-x=platform.system()
-print(x)
+# x=platform.system()
+# print(x)
+
+
+# from mod import greeting as greet
+from mod import *
+greeting("shaikh")
+
+
+# display("looking good")
+
+import pip 
+print(pip.__version__)
